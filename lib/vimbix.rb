e039cb03b326f1da2d10302311136b0a3956c149
Jean-Jacques Martrès <jjmartres@gmail.com>
2013-09-19 21:16:19 +0200

    Fix set root

--- lib/vimbix.rb
+++ lib/vimbix.rb
@@ -3,7 +3,7 @@ require "bundler"
 Bundler.require :default
 
 class VIMbix < Sinatra::Base
-  set :root, Dir[File.dirname(__FILE__) + "/.." ]
+  set :root, Dir[File.dirname(__FILE__) + "/.." ]*""
 end
 
 Dir[File.dirname(__FILE__) + "/vimbix/*.rb"].each { |file| require file }
